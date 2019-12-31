7656e0dfda2009ddf1a72a05ad48b0f85644951b
Klavdija <klavdija@pop-os.localdomain>
2019-12-31 12:56:17 +0100

    health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
+import { NEST_BOOT, NEST_CONSUL } from '@nestcloud/common';
+import { ConsulModule } from '@nestcloud/consul';
+import { ServiceModule } from '@nestcloud/service';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TerminusModule } from '@nestjs/terminus';
+
+import { HealthModule } from './health/health.module';
+import { HealthService } from './health/health.service';
+import { TerminusService } from './health/terminus.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConsulModule.register({ dependencies: [NEST_BOOT] }),
+
+    ServiceModule.register({
+      dependencies: [NEST_CONSUL],
+      service: {
+        id: 'comments-ms',
+        name: 'comments-ms',
+        port: +process.env.PORT,
+      },
+      healthCheck: {
+        timeout: '1s',
+        interval: '10s',
+        deregistercriticalserviceafter: '30s',
+        route: '/health/ready',
+      } as any,
+      maxRetry: 5,
+      retryInterval: 5000,
+    }),
+
+    TerminusModule.forRootAsync({
+      imports: [HealthModule],
+      useClass: TerminusService,
+    }),
+  ],
+  providers: [TerminusService, HealthService],
 })
-export class AppModule {}
+export class AppModule { }
